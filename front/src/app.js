767d63961d594fb5ce90e43c876ecf12c3b9112c
usr1 <usr1@pop-os.localdomain>
2022-09-25 23:14:51 -0700

    super broken

--- front/src/app.js
+++ front/src/app.js
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const store = configureStore()
 
 const jsx = (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <GoogleOAuthProvider clientId="218915995641-pmaencqvs4uqqkhnkngutkvorbec80eh">
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </GoogleOAuthProvider>
 )
 
 const root = createRoot(document.getElementById('app'))
--- front/src/components/pages/LoginPage.js
+++ front/src/components/pages/LoginPage.js
@@ -1,30 +1,26 @@
 import React from 'react'
-
-// function init() {
-//   gapi.load('auth2', function() {
-//     /* Ready. Make a call to gapi.auth2.init or some other API */
-//   });
-// }
-// gapi.auth2.init({
-//   client_id: '218915995641-pmaencqvs4uqqkhnkngutkvorbec80eh.apps.googleusercontent.com'
-// })
-
+import { GoogleLogin } from '@react-oauth/google'
+import jwt_decode from 'jwt-decode'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 class LoginPage extends React.Component {
-  onSignIn = (googleUser) => {
-    const profile = googleUser.getBasicProfile()
-    console.log('id: ', profile.getId())
+  onLogin = ({ credential }) => {
+    let navigate = useNavigate()
+    const decoded = jwt_decode(credential)
+    console.log(decoded.sub)
+    navigate('/dashboard')
   }
-  init() {
-    gapi.load('auth2', function() {
-      /* Ready. Make a call to gapi.auth2.init or some other API */
-    });
-  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <div className="g-signin2" data-onsuccess="onSignIn"></div>
+        <GoogleLogin
+          onSuccess={this.onLogin}
+          onError={(cR) => {
+            console.log("FAILED", cR)
+          }}
+        />
       </div>
     )
   }
